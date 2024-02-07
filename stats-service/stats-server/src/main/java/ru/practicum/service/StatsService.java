eb5125eb0a3c1bacc3ec245eb3e50eccfcfdabdd
Ruslagon <ruslan003.g@yandex.ru>
2024-02-07 04:35:51 +0300

    fix: clearing code

--- stats-service/stats-server/src/main/java/ru/practicum/service/StatsService.java
+++ stats-service/stats-server/src/main/java/ru/practicum/service/StatsService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.db.StatsRepository;
 import ru.practicum.dto.EndpointHitDto;
-import ru.practicum.dto.FormatterLocalDateTime;
 import ru.practicum.model.EndpointHitMapper;
 import ru.practicum.model.ViewStats;
 
